613328dbdb31eb78bf1ff68c8f1f055465109202
Grace Simanullang <gracealvani1@gmail.com>
2025-05-19 09:46:05 +0700

    up

--- app/src/main/java/org/delcom/ifs22022_pam_uas/MainActivity.kt
+++ app/src/main/java/org/delcom/ifs22022_pam_uas/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.*
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.navigation.compose.rememberNavController
 import org.delcom.ifs22022_pam_uas.data.api.ApiClient
 import org.delcom.ifs22022_pam_uas.data.repository.DelcomRepository
-import org.delcom.ifs22022_pam_uas.ui.nav.NavGraph
+import org.delcom.ifs22022_pam_uas.ui.nav.AppNavGraph
 import org.delcom.ifs22022_pam_uas.ui.theme.PamUasTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,9 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             PamUasTheme {
                 val navController = rememberNavController()
-                var token by remember { mutableStateOf("") }
+                var token by rememberSaveable { mutableStateOf("") }
 
-                NavGraph(
+                AppNavGraph(
                     navController = navController,
                     repository = repository,
                     token = token,
@@ -34,6 +34,5 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-class PamUasTheme(function: () -> Unit) {
 
-}
+
